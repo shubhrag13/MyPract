3e627b0b1ea546955b97b757733153709e87a4c1
shubh <shubh@DESKTOP-MDTVO71>
2020-07-15 01:03:57 +0530

    local commit

---
+++ mavenDemo/src/test/java/practiceTest/ExtentReportsLogging.java
@@ -0,0 +1,61 @@
+package practiceTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+public class ExtentReportsLogging {
+	
+	String title;
+	WebDriver driver;
+	ExtentReports report;
+	ExtentTest test;
+	
+	@BeforeMethod
+	public void startUp() {
+		report = new ExtentReports("E:\\Eclipse-Workspace\\mavenDemo\\ExtentReportsTest.HTML");
+		test = report.startTest("ExtentReportsLogging");
+	}
+	
+	@Test
+	public void login() throws Exception {
+		
+		System.setProperty("webdriver.ie.driver", "E:\\Eclipse-Workspace\\Lib\\IEDriverServer.exe");
+		driver = new InternetExplorerDriver();
+		test.log(LogStatus.INFO, "IE is Launched");
+		
+		driver.get("https://www.google.com");
+		Thread.sleep(2000);
+		driver.findElement(By.name("q")).sendKeys("Reporter Class TestNG");
+		test.log(LogStatus.INFO, "Search String Entered");
+		Actions action = new Actions(driver);
+		action.sendKeys(Keys.ENTER).build().perform();
+		test.log(LogStatus.INFO, "Search Performed");
+		
+		Thread.sleep(5000);
+		
+		title = driver.getTitle();
+		test.log(LogStatus.INFO, title);
+		if(title.contains("Reporter")) {
+			test.log(LogStatus.PASS, "Search Results are displayed");
+		}else
+			test.log(LogStatus.FAIL, "An issue occurred while performing search");
+	}
+	
+	@AfterMethod
+	public void end() {
+		report.endTest(test);
+		report.flush();
+		driver.close();
+	}
+
+}
